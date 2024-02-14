441235f2fc420e63de601aa7b511431a900d8c26
aoaomidri <aoaomidori2723@gmail.com>
2024-02-14 15:16:22 +0900

    ぷっしゅ

--- Projects/Game/Src/main.cpp
+++ Projects/Game/Src/main.cpp
@@ -6,6 +6,6 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	std::unique_ptr<Framework> game = std::make_unique<World>();
 
 	game->Execution();
-	//
+
 	return 0;
 }
