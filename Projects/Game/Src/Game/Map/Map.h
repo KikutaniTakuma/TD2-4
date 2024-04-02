3a582dd575116079ec5d5c4ec5d9f94f58100da0
S-Onuki <k022g0020@g.neec.ac.jp>
2024-04-02 17:17:44 +0900

    型用意した

--- Projects/Game/Src/Game/Map/Map.h
+++ Projects/Game/Src/Game/Map/Map.h
@@ -16,6 +16,8 @@ public:
 		kNone,	// 虚空
 		kBox,	// 箱
 	};
+	// 箱の配列 [y][z][x]
+	using MapSize = std::array<std::array<std::array<BoxType, 10u>, 10>, 3u>;
 
 public:
 	Map() = default;
@@ -37,6 +39,11 @@ public:
 	/// @brief 箱のデータをモデルに転送
 	void TransferBoxData();
 
+public:
+	/// @brief 3次元配列の取得
+	/// @return 三次元配列
+	MapSize *GetBlockMap() { return &boxMap_; }
+
 	Vector3 GetGrobalPos(size_t x, size_t y, size_t z) const noexcept
 	{
 		return Vector3{ x * vBoxDistance_->x, y * vBoxDistance_->y, -(z * vBoxDistance_->x) };
@@ -50,7 +57,7 @@ public:
 private:
 
 	// 箱の配列 [y][z][x]
-	std::array<std::array<std::array<BoxType, 10u>, 10>, 3u> boxMap_;
+	MapSize boxMap_;
 	// 箱の数
 	size_t boxCount_;
 
