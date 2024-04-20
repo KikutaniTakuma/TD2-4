c5404add253112707d6291ffc92aea81cb10760a
aoaomidri <aoaomidori2723@gmail.com>
2024-04-20 11:21:41 +0900

    セレクト仮だけど選べるように

--- Projects/Game/Src/Scenes/SelectScene/SelectScene.cpp
+++ Projects/Game/Src/Scenes/SelectScene/SelectScene.cpp
@@ -1,17 +1,29 @@
 #include "SelectScene.h"
 
+bool SelectScene::isStageClear_[maxStage_]{};
+
+int SelectScene::selectNum_ = 0;
+
 SelectScene::SelectScene():
-	BaseScene{ BaseScene::ID::StageSelect }
-{
+	BaseScene{ BaseScene::ID::StageSelect }{
+
 
 }
 
-void SelectScene::Initialize(){
-	currentCamera_->pos = Vector2{ 500.f, 300.f };
+void SelectScene::Initialize() {
 
-	tex_.reset(new Texture2D("./Resources/enemy_popEffect.png"));
-	tex_->scale *= 30.0f;
-	tex_->pos = Vector2{ 500.0f, 0.0f };
+	currentCamera_->pos = Vector2{ 0.f, 0.f };
+	
+
+	for (size_t i = 0; i < texies_.size(); i++) {
+		texies_[i] = std::make_unique<Texture2D>("./Resources/enemy_popEffect.png");
+		texies_[i]->scale *= 60.0f;
+		texies_[i]->pos = Vector2{ -450.0f + stageInterbal * i, 0.0f };
+	}
+
+	selectTex_ = std::make_unique<Texture2D>("./Resources/arrow.png");
+	selectTex_->scale *= 80.0f;
+	selectTex_->pos = { texies_[0]->pos.x, 100.0f };
 }
 
 void SelectScene::Finalize(){
@@ -22,15 +34,48 @@ void SelectScene::Update(){
 	currentCamera_->Debug("カメラ");
 	currentCamera_->Update();
 
-	tex_->Debug("テスト用サークル");
-	tex_->Update();
+
+	for (size_t i = 0; i < texies_.size(); i++) {
+		texies_[i]->color = 0xffffffff;
+		texies_[selectNum_]->color = 0xff0000ff;
+		texies_[i]->Update();
+	}
+	SelectMove();
+
+	selectTex_->pos.x = -450.0f + stageInterbal * selectNum_;
+
+	selectTex_->Debug("テスト用サークル");
+	selectTex_->Update();
+	
+
+
 }
 
 void SelectScene::Draw(){
-	tex_->Draw(currentCamera_->GetViewOthographics());
+	selectTex_->Draw(currentCamera_->GetViewOthographics());
+
+	for (size_t i = 0; i < texies_.size(); i++) {
+		texies_[i]->Draw(currentCamera_->GetViewOthographics());
+	}
 }
 
 void SelectScene::Debug(){
+
+}
+
+void SelectScene::SelectMove(){
+	if (input_->GetKey()->Pushed(DIK_A)){
+		if (selectNum_>0){
+			selectNum_--;
+		}		
+	}
+	else if (input_->GetKey()->Pushed(DIK_D)) {
+		if (selectNum_ < texies_.size() - 1) {
+			selectNum_++;
+		}		
+	}
+
+
 
 }
 
--- Projects/Game/Src/Scenes/SelectScene/SelectScene.h
+++ Projects/Game/Src/Scenes/SelectScene/SelectScene.h
@@ -32,7 +32,31 @@ public:
 	void Draw() override;
 
 	void Debug();
+
+	void SelectMove();
+
+public:
+	static int GetSelectNumber() { return selectNum_; }
+
+	static void SetClearFlag(int x) { isStageClear_[x] = true; }
+
+	static void ClearFlagReset() {
+		for (int i = 0; i < maxStage_ - 1; i++) {
+			isStageClear_[i] = false;
+		}
+	}
 private:
-	std::unique_ptr<Texture2D> tex_;
+	//ステージセレクト用の変数
+	static int selectNum_;
+	int oldSelectNum_ = 0;
+	static const int maxStage_ = 10;
+
+	static bool isStageClear_[maxStage_];
+
+	const float stageInterbal = 100.0f;
+
+	std::array<std::unique_ptr<Texture2D>,maxStage_> texies_;
+
+	std::unique_ptr<Texture2D> selectTex_;
 };
 
