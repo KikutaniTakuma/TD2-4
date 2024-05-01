3e72cae0777d678e64bfca6daf1f68ea64dd4dba
aoaomidri <aoaomidori2723@gmail.com>
2024-05-01 18:53:18 +0900

    画像だけ入れた

--- Projects/Game/Src/Scenes/ResultScene/ResultScene.cpp
+++ Projects/Game/Src/Scenes/ResultScene/ResultScene.cpp
@@ -1,4 +1,18 @@
 #include "ResultScene.h"
+#include "Engine/Graphics/MeshManager/MeshManager.h"
+#include "Engine/Graphics/TextureManager/TextureManager.h"
+#include "AudioManager/AudioManager.h"
+#include "Utils/EngineInfo/EngineInfo.h"
+#include "Math/Quaternion.h"
+#include "Utils/ScreenOut/ScreenOut.h"
+#include <numbers>
+#include <format>
+#include "Engine/Graphics/PipelineObject/WaterPipeline/WaterPipeline.h"
+#include "Game/Water/Water.h"
+#include "Game/Cloud/Cloud.h"
+#include "AudioManager/AudioManager.h"
+#include "Utils/ScreenOut/ScreenOut.h"
+#include "Utils/Random/Random.h"
 
 ResultScene::ResultScene():
 BaseScene{ BaseScene::ID::Result }
