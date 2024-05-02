862a6de184af78118e7afa87e847f45bdcea5a2e
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-05-02 18:36:43 +0900

    totiu

---
+++ Projects/Game/Src/Game/FlaskParticle/FlaskParticle.cpp
@@ -0,0 +1,65 @@
+#include "FlaskParticle.h"
+#include "Drawers/DrawerManager.h"
+#include "Utils/EngineInfo/EngineInfo.h"
+#include "Utils/Random/Random.h"
+
+FlaskParticle::FlaskParticle():
+    emitter_(),
+    freqMin(0.1f),
+    freqMax(0.2f),
+    appParticleNumMin_(5),
+    appParticleNumMax_(10),
+    particles_(100),
+    curentParticleIndex_(0u),
+    isActive_()
+{
+    texture2D_ = DrawerManager::GetInstance()->GetTexture2D();
+    textureID_ = DrawerManager::GetInstance()->LoadTexture("./Resources/Ball.png");
+    scaleMin = Vector3::kIdentity * 20.0f;
+    scaleMax = Vector3::kIdentity * 30.0f;
+}
+
+void FlaskParticle::Update() {
+    if (isActive_) {
+        float deltatime = Lamb::DeltaTime();
+
+        uint32_t appParticle = Lamb::Random(appParticleNumMin_, appParticleNumMax_);
+
+        for (size_t index = curentParticleIndex_; index < (curentParticleIndex_ + appParticle) and index < particles_.size(); index++) {
+            particles_[index].isActive = true;
+            particles_[index].activeTime = 0.0f;
+            particles_[index].scale = Lamb::Random(scaleMin, scaleMax);
+        }
+    }
+}
+
+void FlaskParticle::Draw(const Mat4x4& camera) {
+    for (auto& particle : particles_) {
+        if (particle.isActive) {
+            texture2D_->Draw(
+                Mat4x4::MakeAffin(particle.scale, Vector3::kZero, particle.translate),
+                Mat4x4::kIdentity,
+                camera,
+                textureID_,
+                0xffffffff,
+                BlendType::kNormal
+            );
+        }
+    }
+}
+
+void FlaskParticle::Start() {
+    
+}
+
+void FlaskParticle::Restart() {
+    
+}
+
+void FlaskParticle::Stop() {
+    
+}
+
+void FlaskParticle::Pause() {
+    
+}
--- Projects/Game/Src/Game/FlaskParticle/FlaskParticle.h
+++ Projects/Game/Src/Game/FlaskParticle/FlaskParticle.h
@@ -1,22 +1,102 @@
 #pragma once
-#include "Math/Vector2.h"
+#include "Math/Vector3.h"
+#include "Math/Mat4x4.h" 
+#include "Utils/Flg/Flg.h"
+#include <vector>
+#include "Utils/SafePtr/SafePtr.h"
+#include "Drawers/Texture2D/Texture2D.h"
+
+// xが真ん中まで来たらx軸の移動を止める
+// yは目的のとこまで移動して目的のとこまで行ったら殺す
+
 
 class FlaskParticle {
-private:
+public:
 	struct Emitter {
-		Vector2 translate;
-		Vector2 scale;
+		Vector3 translate;
+		Vector3 scale;
 
+		float maxRotate_;
+		float minRotate_;
 	};
 
-	struct ParticleStatus
-	{
-		Vector2 translate;
-		Vector2 scale;
+	struct ParticleStatus {
+		Vector3 translate;
+		Vector3 scale;
+
+		float activeTime = 0.0f;
+
+		bool isActive;
 	};
+
+#ifndef NORETURN
+#define NORETURN [[noreturn]] void
+#endif // !NORETURN
+
 
 public:
-	// xが真ん中まで来たらx軸の移動を止める
-	// yは目的のとこまで移動して目的のとこまで行ったら殺す
+	FlaskParticle();
+	FlaskParticle(const FlaskParticle&) = default;
+	FlaskParticle(FlaskParticle&&) = default;
+	~FlaskParticle() = default;
 
+public:
+	NORETURN Update();
+	NORETURN Draw(const Mat4x4& camera);
+
+public:
+	NORETURN Start();
+
+	NORETURN Restart();
+
+	NORETURN Stop();
+
+	NORETURN Pause();
+
+public:
+	NORETURN SetEndTranslate(const Vector3& endTranslate) {
+		endTranslate_ = endTranslate;
+	}
+
+	NORETURN Resize(size_t size) {
+		particles_.resize(size);
+	}
+
+	NORETURN SetEmitter(const Emitter& emitter) {
+		emitter_ = emitter;
+	}
+
+	[[nodiscard]] const Lamb::Flg& GetIsActive() const {
+		return isActive_;
+	}
+
+private:
+	Emitter emitter_;
+
+	float freqMin;
+	float freqMax;
+
+	uint32_t appParticleNumMin_;
+	uint32_t appParticleNumMax_;
+
+	Vector3 scaleMin;
+	Vector3 scaleMax;
+
+	float deathTime_;
+
+	std::vector<ParticleStatus> particles_;
+	uint32_t curentParticleIndex_;
+
+
+	Vector3 endTranslate_;
+
+	Lamb::Flg isActive_;
+
+	Lamb::SafePtr<Texture2D> texture2D_;
+	uint32_t textureID_;
+
+private:
+#ifdef NORETURN
+#undef NORETURN
+#endif // NORETURN
 };
