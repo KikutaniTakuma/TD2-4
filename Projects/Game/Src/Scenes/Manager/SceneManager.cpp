4e297ad493741bd9aefe3bba98072ff287a7e452
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-05-12 13:53:46 +0900

    Fadeが映らない

--- Projects/Game/Src/Scenes/Manager/SceneManager.cpp
+++ Projects/Game/Src/Scenes/Manager/SceneManager.cpp
@@ -15,6 +15,7 @@ void SceneManager::Initialize(std::optional<BaseScene::ID> firstScene, std::opti
 
 
 	fade_ = std::make_unique<Fade>();
+	fadeCamera_.pos.x = -10.0f;
 	fadeCamera_.Update();
 
 	frameInfo_ = FrameInfo::GetInstance();
