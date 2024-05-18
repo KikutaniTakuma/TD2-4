db2094ba306a1be28e9d879314c19ac8153015d4
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-05-18 17:12:46 +0900

    fade実装

--- Projects/Game/Src/Scenes/Manager/Fade/Fade.cpp
+++ Projects/Game/Src/Scenes/Manager/Fade/Fade.cpp
@@ -9,14 +9,48 @@ Fade::Fade() :
 	isInStart_(false),
 	isOutStart_(false),
 	tex_(),
-	transform(),
+	transform_(),
 	color_(std::numeric_limits<uint32_t>::max()),
-	fadeTime_(0.75f)
+	fadeTime_(1.5f)
 {
 	tex_ = DrawerManager::GetInstance()->GetTexture2D();
-	transform = std::make_unique<Transform>();
-	transform->scale = Lamb::ClientSize();
-	transform->translate.z = -10.0f + static_cast<float>(10e-5);
+	transform_ = std::make_unique<Transform>();
+	transform_->scale = Lamb::ClientSize();
+	transform_->translate.z = 0.0f;
+
+	Vector2 offset = Lamb::ClientSize() / 5.0f;
+	float scale = Lamb::ClientSize().x / 5.0f * 3.0f;
+	for (float index = -2.0f; auto & i : slimeTransform_) {
+		i = std::make_unique<Transform>();
+		i->scale *= scale;
+		index++;
+	}
+	slimePositions_[0].first.x = offset.x * -2.0f;
+	slimePositions_[1].first.x = offset.x * 2.0f;
+	slimePositions_[2].first.x = offset.x * -1.0f;
+	slimePositions_[3].first.x = offset.x * 1.0f;
+	slimePositions_[4].first.x = offset.x * 0.0f;
+
+	slimePositions_[0].second.x = offset.x * -2.0f;
+	slimePositions_[1].second.x = offset.x * 2.0f;
+	slimePositions_[2].second.x = offset.x * -1.0f;
+	slimePositions_[3].second.x = offset.x * 1.0f;
+	slimePositions_[4].second.x = offset.x * 0.0f;
+
+	slimePositions_[0].first.y = offset.y * -2.0f - Lamb::ClientSize().y;
+	slimePositions_[1].first.y = offset.y * -2.0f - Lamb::ClientSize().y;
+	slimePositions_[2].first.y = offset.y * -1.0f - Lamb::ClientSize().y;
+	slimePositions_[3].first.y = offset.y * -1.0f - Lamb::ClientSize().y;
+	slimePositions_[4].first.y = offset.y *  0.0f - Lamb::ClientSize().y;
+
+	slimePositions_[0].second.y = offset.y *  2.0f + Lamb::ClientSize().y;
+	slimePositions_[1].second.y = offset.y *  2.0f + Lamb::ClientSize().y;
+	slimePositions_[2].second.y = offset.y *  1.0f + Lamb::ClientSize().y;
+	slimePositions_[3].second.y = offset.y *  1.0f + Lamb::ClientSize().y;
+	slimePositions_[4].second.y = offset.y *  0.0f + Lamb::ClientSize().y;
+
+
+	slimeTexID_ = DrawerManager::GetInstance()->LoadTexture("./Resources/Enemy/slimeWallWalk.png");
 }
 
 void Fade::OutStart() {
@@ -65,6 +99,24 @@ void Fade::Update() {
 	}
 
 	ease_.Update();
+	transform_->CalcMatrix();
+
+	for (size_t i = 0; i < kSlimeNum; i++) {
+		if (isInStart_) {
+			slimeTransform_[i]->translate = Vector3::Lerp(slimePositions_[i].second, slimePositions_[i].first, ease_.GetT());
+		}
+		else if (isOutStart_) {
+			slimeTransform_[i]->translate = Vector3::Lerp(slimePositions_[i].first, slimePositions_[i].second, ease_.GetT());
+		}
+		slimeTransform_[i]->CalcMatrix();
+	}
+
+#ifdef _DEBUG
+	ImGui::Begin("fade");
+	transform_->ImGuiWidget("Fade");
+	ImGui::End();
+#endif // _DEBUG
+
 
 	if (ease_.ActiveExit()) {
 		isInStart_ = false;
@@ -72,7 +124,16 @@ void Fade::Update() {
 	}
 }
 void Fade::Draw([[maybe_unused]]const Mat4x4& viewProjection) {
-	if (isInStart_ || isOutStart_) {
-		tex_->Draw(transform->matWorld_, Mat4x4::kIdentity, viewProjection, 0u, color_, BlendType::kNormal);
+	if (isInStart_ or isOutStart_) {
+		tex_->Draw(transform_->matWorld_, Mat4x4::kIdentity, viewProjection, 0u, color_, BlendType::kUnenableDepthNormal);
+
+
+		for (auto& i : slimeTransform_) {
+			tex_->Draw(
+				i->matWorld_,
+				Mat4x4::MakeScalar(Vector3::kXIdentity * 0.333f + Vector3::kYIdentity * (isInStart_ ? -1.0f : 1.0f)),
+				viewProjection, slimeTexID_, std::numeric_limits<uint32_t>::max(), BlendType::kUnenableDepthNormal
+			);
+		}
 	}
 }
--- Projects/Game/Src/Scenes/Manager/SceneManager.cpp
+++ Projects/Game/Src/Scenes/Manager/SceneManager.cpp
@@ -15,7 +15,7 @@ void SceneManager::Initialize(std::optional<BaseScene::ID> firstScene, std::opti
 
 
 	fade_ = std::make_unique<Fade>();
-	fadeCamera_.pos.x = -10.0f;
+	fadeCamera_.pos.z = -10.0f;
 	fadeCamera_.Update();
 
 	frameInfo_ = FrameInfo::GetInstance();
@@ -135,7 +135,6 @@ void SceneManager::Update() {
 void SceneManager::Draw() {
 	if (scene_) {
 		scene_->Draw();
-		
 	}
 
 	fade_->Draw(fadeCamera_.GetViewOthographics());
