3686338cb11a2fdaff4dc208d9a5735100a27fa6
k022g0020 <k022g0020@g.neec.ac.jp>
2024-05-23 00:35:32 +0900

    ステージセレクトを最初にするように

--- Projects/Game/Src/World/World.cpp
+++ Projects/Game/Src/World/World.cpp
@@ -33,7 +33,7 @@ void World::Initialize() {
 	// シーンマネージャー初期化
 	sceneManager_ = std::make_unique<SceneManager>();
 
-	sceneManager_->Initialize(BaseScene::ID::Game, BaseScene::ID::Game);
+	sceneManager_->Initialize(BaseScene::ID::StageSelect, BaseScene::ID::StageSelect);
 
 	ParticleEditor::Initialize();
 	particleEditor_ = ParticleEditor::GetInstance();
