5752c2cd82af16535a0010578eafe1e358705cf0
KikutaniTakuma <k022g0126@g.neec.ac.jp>
2024-05-27 01:00:23 +0900

    ダイアモンドに変更

--- Projects/Game/Src/Scenes/SelectToGame/SelectToGame.cpp
+++ Projects/Game/Src/Scenes/SelectToGame/SelectToGame.cpp
@@ -7,7 +7,7 @@ SelectToGame::SelectToGame() {
 	isStageClear_ = {};
 
 	Lamb::SafePtr texManager = TextureManager::GetInstance();
-	stageItemTextureIDs[0] = texManager->LoadTexture("./Resources/UI/Item/gold.png"); // ゴールド
+	stageItemTextureIDs[0] = texManager->LoadTexture("./Resources/UI/Item/diamond.png"); // でぇーあもんど
 	stageItemTextureIDs[1] = texManager->LoadTexture("./Resources/UI/Item/pendant.png"); // ペンダント
 	stageItemTextureIDs[2] = texManager->LoadTexture("./Resources/UI/product.png"); // 未定
 	stageItemTextureIDs[3] = texManager->LoadTexture("./Resources/UI/Item/poisonPotion.png"); // ポーション
