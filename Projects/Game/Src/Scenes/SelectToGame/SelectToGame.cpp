c3b105b33d4d0b66c2f782749388b04fe08f78a5
aoaomidri <aoaomidori2723@gmail.com>
2024-05-27 12:58:53 +0900

    アイテム表示の変更、エリクサー、人間の追加

--- Projects/Game/Src/Scenes/SelectToGame/SelectToGame.cpp
+++ Projects/Game/Src/Scenes/SelectToGame/SelectToGame.cpp
@@ -9,12 +9,12 @@ SelectToGame::SelectToGame() {
 	Lamb::SafePtr texManager = TextureManager::GetInstance();
 	stageItemTextureIDs[0] = texManager->LoadTexture("./Resources/UI/Item/diamond.png"); // でぇーあもんど
 	stageItemTextureIDs[1] = texManager->LoadTexture("./Resources/UI/Item/pendant.png"); // ペンダント
-	stageItemTextureIDs[2] = texManager->LoadTexture("./Resources/UI/product.png"); // 未定
+	stageItemTextureIDs[2] = texManager->LoadTexture("./Resources/UI/Item/orb.png"); // 未定
 	stageItemTextureIDs[3] = texManager->LoadTexture("./Resources/UI/Item/poisonPotion.png"); // ポーション
 	stageItemTextureIDs[4] = texManager->LoadTexture("./Resources/UI/Item/ring.png"); // 指輪
-	stageItemTextureIDs[5] = texManager->LoadTexture("./Resources/UI/Item/orb.png"); // オーブ
-	stageItemTextureIDs[6] = texManager->LoadTexture("./Resources/UI/Item/sord.png"); // 剣
-	stageItemTextureIDs[7] = texManager->LoadTexture("./Resources/UI/product.png");   // 未定
+	stageItemTextureIDs[5] = texManager->LoadTexture("./Resources/UI/Item/sord.png"); // オーブ
+	stageItemTextureIDs[6] = texManager->LoadTexture("./Resources/UI/Item/elixir.png"); // 剣
+	stageItemTextureIDs[7] = texManager->LoadTexture("./Resources/UI/Item/human.png");   // 未定
 
 }
 
