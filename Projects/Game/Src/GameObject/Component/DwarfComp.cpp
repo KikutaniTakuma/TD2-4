07ea44060acaaa15a787b490a552732394b0fb0f
S-Onuki <k022g0020@g.neec.ac.jp>
2024-05-29 16:05:10 +0900

    常にプレイヤ側を向き続けるように変更

--- Projects/Game/Src/GameObject/Component/DwarfComp.cpp
+++ Projects/Game/Src/GameObject/Component/DwarfComp.cpp
@@ -151,24 +151,24 @@ void DwarfComp::ChangeMovementTarget()
 
 void DwarfComp::FireBullet()
 {
-
 	timer_.Update(GetDeltaTime());
+	Lamb::SafePtr player = GameManager::GetInstance()->GetPlayer();
+	if (player) {
+
+		Lamb::SafePtr plBody = player->GetComponent<LocalBodyComp>();
+		facing_ = static_cast<int32_t>(SoLib::Math::Sign(plBody->localPos_.x - pLocalBodyComp_->localPos_.x));
+	}
 	if (not timer_.IsActive()) {
-		Lamb::SafePtr player = GameManager::GetInstance()->GetPlayer();
-		if (player) {
-			Lamb::SafePtr plBody = player->GetComponent<LocalBodyComp>();
 
-			facing_ = static_cast<int32_t>(SoLib::Math::Sign(plBody->localPos_.x - pLocalBodyComp_->localPos_.x));
 
-			timer_.Start(vBulletFireSpan_);
+		timer_.Start(vBulletFireSpan_);
 
-			Lamb::SafePtr pGManager = GameManager::GetInstance();
+		Lamb::SafePtr pGManager = GameManager::GetInstance();
 
-			if (facing_) {
-				const Vector2 facingVec = Vector2::kXIdentity * static_cast<float>(facing_);
+		if (facing_) {
+			const Vector2 facingVec = Vector2::kXIdentity * static_cast<float>(facing_);
 
-				pGManager->AddEnemyBullet(pLocalBodyComp_->localPos_ + facingVec, Vector2::kXIdentity * (facing_ * vBulletSpeed_));
-			}
+			pGManager->AddEnemyBullet(pLocalBodyComp_->localPos_ + facingVec, Vector2::kXIdentity * (facing_ * vBulletSpeed_));
 		}
 	}
 }
